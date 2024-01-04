85208722d8cf74e7886651c827f95d8227494298
amitsevda2303 <rohitkmt88@gmail.com>
2024-01-04 19:53:29 +0530

    home path navigation block if usertoken is not available

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const HomePage = () => {
+  const navigate = useNavigate()
+  const userToken = localStorage.getItem("User")
+
+  useEffect(() => {
+    if (!userToken) {
+      navigate('/')
+    }
+  }, []);
+
   return (
     <div style={{width:"100vw",height:"100vh",backgroundColor:"black",color:"white"}}>HomePage</div>
   )
